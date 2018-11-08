1bd0c132fdfa94e26f2862ca36a338b743f32de9
Prince Ofori <princeofori065@gmail.com>
2018-11-08 09:15:19 +0000

    getTime() corrected

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message.js')
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname + './../public');
@@ -19,17 +20,9 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected')
     
-    socket.emit('newMessage', {
-            from: 'Admin',
-            text: 'Hello there, welcome to our chat app',
-            createdAt: new Date().getTime()
-        })
+    socket.emit('newMessage', generateMessage('Admin', 'Hello there welcome to our chat app'))
     
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (outbox) => {
         console.log(outbox);
